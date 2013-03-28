6293042ad9646a18afe2346ed0bcb3f0611ec3c7
LimpTeaM <limp@limpteam.ru>
2013-03-28 12:51:30 +0400

    edit geocoder, upload icon

--- geocoder.js
+++ geocoder.js
@@ -9,10 +9,11 @@ function ad() {
 };
 
 function onLocationFound(e) {
+    var youhereicon = L.icon({iconUrl:'img/male-user.png', iconAnchor:   [10, 80],  popupAnchor:  [5, -75]});
     var radius = e.accuracy / 2;
     var lat = e.latlng.lat;
     var lon = e.latlng.lng;
-    L.marker(e.latlng).addTo(map).bindPopup('Вы где-то здесь. В пределах '+radius+' метров').openPopup();
+    L.marker(e.latlng, {icon: youhereicon}).addTo(map).bindPopup('Вы где-то здесь. В пределах '+radius+' метров').openPopup();
     //L.circle(e.latlng, radius).addTo(map);
     $.getJSON('http://nominatim.openstreetmap.org/search?format=json&limit=1&q='+lat+' '+lon, function(data) {
         var items = [];
